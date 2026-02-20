4d0d7df6c3e8a3d21179e91d3eecee7d3bab22d5
Ritun Chakraborty <mithynead@gmail.com>
2026-02-20 07:34:54 +0530

    updated-v Agent-II

--- Leran_1/Agent_part.py
+++ Leran_1/Agent_part.py
@@ -100,6 +100,17 @@ while user_input1 != "exit":
 
         user_input1 = input("Enter-:")
 
+with open("logging.txt", "w") as file:
+        file.write("Your Conversation Log:\n")
+        for message in conversation_history:
+                if isinstance(message, HumanMessage):
+                        file.write(f"You: {message.content}\n")
+                elif isinstance(message, AIMessage):
+                        file.write(f"AI: {message.content}\n\n")
+        file.write("End of Conversation")
+
+print("Conversation saved to logging.txt")
+
 
 
 
